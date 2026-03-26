8169c68ea20950bdf08f67bed3f70a62506d1651
momenmotaz <momenmotaz88@gmail.com>
2026-03-26 06:21:24 +0200

    push

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+import os
 from fastapi import FastAPI
 from api.routers import router
 from core.config import settings
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger("startup")
 
 app = FastAPI(
     title=settings.PROJECT_NAME, 
@@ -10,6 +15,17 @@ app = FastAPI(
 
 # Connect the routers
 app.include_router(router)
+
+@app.on_event("startup")
+async def startup_event():
+    port = os.environ.get("PORT", default="8000")
+    logger.info("=====================================================")
+    logger.info(f"🚀 PDF Extract Service Started on Port {port}")
+    logger.info("=====================================================")
+    logger.info(f"✅ Health Check endpoint   : GET  http://0.0.0.0:{port}/health")
+    logger.info(f"📄 PDF Extract endpoint    : POST http://0.0.0.0:{port}/extract-pdf")
+    logger.info(f"📘 Swagger Documentation   : GET  http://0.0.0.0:{port}/docs")
+    logger.info("=====================================================")
 
 if __name__ == "__main__":
     import uvicorn
